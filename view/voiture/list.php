af0ef993ecf3993f2bf8574a7bc7ae6a1bf26821
Yves DANIEL <yves.daniel11@gmail.com>
2017-11-14 13:15:09 +0100

    TP7

--- view/voiture/list.php
+++ view/voiture/list.php
@@ -12,6 +12,7 @@
 		echo "<p>Il n'y a pas de cookie login</p>";
 	}
 	foreach ( $tab_v as $k ) {
+
 		echo '<p> Voiture d\'immatriculation <a href=\'./index.php?controller=voiture&action=read&'.ModelVoiture::getPrimary () .'=' . rawurlencode ( $k -> getImmatriculation () ) . '\'>' . htmlspecialchars ( $k -> getImmatriculation () ) . '</a>.</p>';
 	}
 ?>
